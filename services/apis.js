65d88f4c840e1897bb90ed83db71c974d9d5c1bb
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2018-02-06 08:55:33 -0800

    Add fixer.io caching

--- services/apis.js
+++ services/apis.js
@@ -1,14 +1,23 @@
-const Promise = require("es6-promise").Promise
+const Promise = require('es6-promise').Promise
 const fetch = require('node-fetch');
+const Cache = require('lite-node-cache');
+
+const fixerCache = new Cache({
+    ttl: 30000
+});
 
 module.exports = class APIs {
   static fetch() {
     return Promise.all([
-      fetch('https://api.fixer.io/latest?base=USD&symbols=AUD'),
       fetch('https://api.gdax.com/products/BTC-USD/ticker'),
       fetch('https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xbt&secondaryCurrencyCode=aud')
-    ]).then((results) => Promise.all(results.map((result) => result.json()))
-  ).then(([fixer, gdax, independentReserve]) => {
+    ])
+    .then((results) => Promise.all(results.map((result) => result.json())))
+    .then(([gdax, independentReserve]) => {
+      return this.getExchangeRate().then((fixer) => {
+        return [fixer, gdax, independentReserve];
+      });
+    }).then(([fixer, gdax, independentReserve]) => {
       if (!fixer.rates.AUD ||
         !gdax.bid ||
         !gdax.ask ||
@@ -29,5 +38,22 @@ module.exports = class APIs {
       console.log(error);
       return Promise.reject(error);
     });
+  }
+
+  static getExchangeRate() {
+    const cacheItem = fixerCache.get('fixer');
+    if (cacheItem) {
+      return Promise.resolve(cacheItem);
+    } else {
+      return fetch('https://api.fixer.io/latest?base=USD&symbols=AUD')
+      .then((result) => result.json())
+      .then((fixer) => {
+        fixerCache.set('fixer', fixer);
+        return fixer;
+      }).catch((error) => {
+        console.log('fixer fetch failed');
+        console.log(error);
+      });
+    }
   }
 }
