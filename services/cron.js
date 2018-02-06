72c8bf02eab373a827ebf92601e9186e6c1907cc
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2018-02-05 18:35:11 -0800

    Add push notification work (not finished)

--- services/cron.js
+++ services/cron.js
@@ -1,10 +1,12 @@
 const later = require('later');
 const APIs = require('./apis');
 const DB = require('../db');
+const PushBullet = require('pushbullet');
 
 class Cron {
   start() {
     this.fetchData();
+    this._pushbullet = new PushBullet(process.env.PUSHBULLET_KEY);
     later.setInterval(this.fetchData.bind(this), later.parse.text('every 1 min'));
   }
 
@@ -18,9 +20,41 @@ class Cron {
         jsonResults.askAUD,
         jsonResults.exchangeRate
       );
+      this.checkSubscriptions(
+        jsonResults.bidUSD,
+        jsonResults.askUSD,
+        jsonResults.bidAUD,
+        jsonResults.askAUD,
+        jsonResults.exchangeRate
+      );
     }).catch((error) => {
       console.log(`failure fetching data at interval: ${Date.now()}`);
       console.log(error);
+    });
+  }
+
+  checkSubscriptions(bidUSD, askUSD, bidAUD, askAUD, exchangeRate) {
+    const diffs = {
+      'market': (bidAUD / askUSD / exchangeRate - 1) * 100,
+      'limit': (askAUD / bidUSD / exchangeRate - 1) * 100
+    };
+    DB.getSubscriptions().then((subscriptions) => {
+      subscriptions
+        .filter((subscription) => (subscription.isHigher && diffs[subscription.type] > subscription.value) ||
+                (!subscription.isHigher && diffs[subscription.type] < subscription.value))
+        .forEach((subscription) => {
+          this._pushbullet.link(
+            process.env.PUSHBULLET_DEVICE,
+            'Arbitrage Notification',
+            process.env.DOMAIN,
+            `${diffs['market']}% (${subscription.type}) crossed threshold ${subscription.value}`,
+            (error, response) => {
+              error && console.log(error);
+              console.log(response);
+              DB.removeSubscription(subscription);
+            }
+          );
+        });
     });
   }
 }
